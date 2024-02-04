ddb320dc4e6fd9b420c47420f7406a1d47497048
Duguce <zhgyqc@163.com>
2024-02-04 21:49:08 +0800

    fix: modify the load model from huggingface

--- core/llm/hf.py
+++ core/llm/hf.py
@@ -6,24 +6,27 @@ from core.llm.base import BaseLLM
 
 class AquilaChat2_7B(BaseLLM):
     # For a more formal inference approach, please refer to https://huggingface.co/BAAI/AquilaChat2-7B.
-    def _request(self, query: str) -> str:
+    def __init__(self):
+        super().__init__()
         local_path = self.conf['url']
-        tokenizer = AutoTokenizer.from_pretrained(
+        self.tokenizer = AutoTokenizer.from_pretrained(
             local_path, use_fast=False, trust_remote_code=True)
-        model = AutoModelForCausalLM.from_pretrained(
+        self.model = AutoModelForCausalLM.from_pretrained(
             local_path,
             device_map="auto",
             torch_dtype=torch.bfloat16,
-            trust_remote_code=True)
-        gen_kwargs = {
+            trust_remote_code=True).eval()
+        self.gen_kwargs = {
             "temperature": self.params['temperature'],
             "do_sample": True,
             "max_new_tokens": self.params['max_new_tokens'],
             "top_p": self.params['top_p'],
             "top_k": self.params['top_k'],
         }
-        input_ids = tokenizer.encode(query, return_tensors="pt").cuda()
-        output = model.generate(input_ids, **gen_kwargs)[0]
-        response = tokenizer.decode(
+
+    def _request(self, query: str) -> str:
+        input_ids = self.tokenizer.encode(query, return_tensors="pt").cuda()
+        output = self.model.generate(input_ids, **self.gen_kwargs)[0]
+        response = self.tokenizer.decode(
             output[len(input_ids[0]) - len(output):], skip_special_tokens=True)
         return response
